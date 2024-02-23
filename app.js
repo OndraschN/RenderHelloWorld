10f9a94f72e7ece833052e0409635b0551ac5247
André Niepraschk <andre.niepraschk@abatplus.de>
2024-02-23 18:18:37 +0100

    add some routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 app.get("/test", (req, res) => res.type('html').send(test));
 
+app.get("/contact", (req, res) => res.type('html').send(contact));
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
@@ -33,6 +35,19 @@ const test = `
   <body>
     <section>
       This is the test route!
+    </section>
+  </body>
+</html>
+`
+const contact = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Test from Render! First page from Ondrasch</title>
+  </head>
+  <body>
+    <section>
+      <h1>This is the contact route!</h1>
     </section>
   </body>
 </html>
